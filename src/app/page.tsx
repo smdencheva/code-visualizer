a5d966def60f4a951ef498995867dbc6c6a05f10
Stanislava Dencheva <smdencheva@gmail.com>
2025-02-21 13:28:54 +0200

    Add code input component to main structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,29 @@
 'use client';
 
-import CodeInput from '@/components/code-input/code-input';
-import ForLoop from '@/components/for-loop/for-loop';
-import GenericLoop from '@/components/generic-loop/generic-loop';
-import IfElseStatement from '@/components/if-else-statements/if-else-statements';
-import Switch from '@/components/switch/switch';
-import FoorLoop from '@/components/for-loop/for-loop';
 import CodeParserInputComponent from '@/components/code-input/code-input';
+import React from 'react';
 
 export default function Home() {
   const handleTextChange = (text: string) => {
     console.log('Text changed:', text);
   };
   return (
-    <div className="font-mono">
-      <header className="bg-slate-300 p-5">
-        <h1 className="text-center font-bold">Code Visualizer</h1>
-      </header>
-      <main className="bg-slate-100 p-5 flex flex-row">
-        <div className="basis-1/2 p-5">
-          <CodeParserInputComponent />
+    <div>
+      <div className="bg-slate-100 p-5 flex flex-row">
+        <div>
+            <h1>Programming Problem Solutions</h1>
+          <section>
+            <p className="paragraph">
+              Welcome to our page where we showcase a variety of solutions to common programming challenges.
+              Here you'll find detailed explanations, code examples, and best practices to help you tackle your coding problems.
+              If you want to test a problem of your own, paste it bellow.
+            </p>
+          </section>
+          <section className="example">
+            <CodeParserInputComponent />
+          </section>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center bg-slate-800 p-5 text-white"></footer>
+      </div>
     </div>
   );
 }
